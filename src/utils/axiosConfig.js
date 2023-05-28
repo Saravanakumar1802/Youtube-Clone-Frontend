6bae949eaba6890e4293fe19c32a0afe5608db48
Saravanakumar1802 <saravana1802kumar@gmail.com>
2023-05-28 13:19:05 +0530

    commit

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:7001/api",
+  baseURL: "https://youtube-clone-backend-sk.onrender.com/api",
   withCredentials: true,
 });
+
+// baseURL: "http://localhost:7001/api"
